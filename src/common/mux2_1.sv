6ba01b41771e814ec9fcba9ca2a75a0df8a19667
Zixiang Li <lzx97@outlook.com>
2018-02-08 03:18:57 -0800

    Fixed timescale issue

--- src/common/mux2_1.sv
+++ src/common/mux2_1.sv
@@ -1,5 +1,3 @@
-`timescale 1ns/10ps
-
 module mux2_1 (out, w0, w1, sel);
 	input w0, w1;
 	input sel;
@@ -9,8 +7,8 @@ module mux2_1 (out, w0, w1, sel);
 	
 	not inverter (inverter_o, sel);
 	
-	and #50 and_gate1(And1_o, w0, inverter_o);
-	and #50 and_gate2(And2_o, w1, sel);
+	and and_gate1(And1_o, w0, inverter_o);
+	and and_gate2(And2_o, w1, sel);
 	
 	or or_gate(out, And1_o, And2_o);
 	
