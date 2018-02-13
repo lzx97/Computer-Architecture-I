898034f5b2ebf626303d021512725b38c3dfa23c
Zixiang Li <lzx97@outlook.com>
2018-02-13 15:03:53 -0800

    Finished program counter

---
+++ src/cpu_single/pc.sv
@@ -0,0 +1,8 @@
+module pc(addr_out, addr_in, enable, clk, rst);
+    input [63:0] addr_in;
+    input enable, clk, rst;
+    output [63:0] addr_out;
+
+    register PC (.data_out(addr_out), .data_in(addr_in), .enable, .clk, .rst);
+    
+endmodule 
