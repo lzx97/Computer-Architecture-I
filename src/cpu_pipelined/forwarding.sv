82df8ce58ad68681d9cafe1b2a2737f46c35c5f6
Zixiang Li <liz66@uw.edu>
2018-02-24 20:49:18 -0800

    Finished forwarding

--- src/cpu_pipelined/forwarding.sv
+++ src/cpu_pipelined/forwarding.sv
@@ -1,13 +1,38 @@
-module forwaring();
+module forwarding(ForwardA, ForwardB, Rn, Rd, Rd_EX_MEM, Rd_MEM_WB, EX_MEM_RegWrite, MEM_WB_RegWrite);
 	input [4:0] Rn, Rd;
-	input [4:0] Rd_EX_MEM, Rd_MEM_WBl;
-	input []  , cntrl_MEM_WB; // fix width?
-	output [1:0] muxcntrl0, muxcntrl1;
+	input [4:0] Rd_EX_MEM, Rd_MEM_WB;
+	input EX_MEM_RegWrite, MEM_WB_RegWrite;
+	output reg [1:0] ForwardA, ForwardB;
 	
 	always_comb begin
+		if (EX_MEM_RegWrite && (Rd_EX_MEM != 5'b11111) && (Rd_EX_MEM == Rn)) begin
+			ForwardA = 2'b10;
+			ForwardB = 2'b00;
+		end
 		
-	
-	
+		else if (EX_MEM_RegWrite && (Rd_EX_MEM != 5'b11111) && (Rd_EX_MEM == Rm)) begin
+			ForwardA = 2'b00;
+			ForwardB = 2'b10;
+		end
+		
+		else if (MEM_WB_RegWrite && Rd_MEM_WB != 5'b11111 && 
+				!(EX_MEM_RegWrite && (Rd_EX_MEM != 5'b11111) && (Rd_EX_MEM != Rn)) &&
+					(Rd_MEM_WB == Rn)) begin
+			ForwardA = 2'b01;
+			ForwardB = 2'b00;
+		end
+		
+		else if (MEM_WB_RegWrite && Rd_MEM_WB != 5'b11111 && 
+				!(EX_MEM_RegWrite && (Rd_EX_MEM != 5'b11111) && (Rd_EX_MEM != Rm)) &&
+					(Rd_MEM_WB == Rm)) begin
+			ForwardA = 2'b00;
+			ForwardB = 2'b01;
+		end
+		
+		else begin
+			ForwardA = 2'b00;
+			ForwardB = 2'b00;
+		end 
 	end
 	
 
